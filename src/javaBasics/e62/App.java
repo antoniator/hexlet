3edd863cce31358274bac9122602e80424fb63d0
Asterroth <anton.malukhin@gmail.com>
2023-09-15 01:27:06 +0300

    доб. задание 62, решение препода в комментарии

--- src/javaBasics/e62/App.java
+++ src/javaBasics/e62/App.java
@@ -20,4 +20,24 @@ public class App {
 
         return result;
     }
+
+    /*
+    Решение препода:
+
+    public class App {
+    public static String encrypt(String str) {
+        // BEGIN
+        var result = "";
+        for (var i = 0; i < str.length(); i += 2) {
+            var nextSymbol = (i + 2 > str.length()) ? "" : str.charAt(i + 1);
+            result = result + nextSymbol + str.charAt(i);
+        }
+
+        return result;
+        // END
+    }
+}
+
+     */
+
 }
